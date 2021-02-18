a0ad9679d9cee02b24fbb55cced7ae7deaa0ff7f
Jack Dolgin <jack.dolgin@duke.edu>
2021-02-18 00:50:44 -0600

    Add comments to audio recording file

--- online-task/js/record-audio.js
+++ online-task/js/record-audio.js
@@ -3,6 +3,23 @@
 let mediaRecorder, audioStartTime;
 
 function startAudio(expectingCompletion) {
+
+    /*
+   You can call this function with `startAudio(true)` or `startAudio(false)`
+   The boolean parameter indicates whether to display a progress bar while
+   the participant is waiting for the data to upload to the server
+
+   Calling the function begins the recording, but it won't upload the data
+   until the recording ends. To end the recording, call `mediaRecorder.stop()`
+
+   In theory the variable `audioStartTime` is supposed to indicate when the
+   recording began. However, I've found that there may be a delay between
+   when the function is called and when the actual recording starts. My
+   workaround in my analyses is just to the time in the experiment when
+   the recording ends (the last trial), rather than when the recording
+   begins, as my reference point. It might also be worth adding a short
+   delay between when you start the recording and when the first trial begins.
+   */
   
   navigator.mediaDevices.getUserMedia({ audio: true })
     .then(stream => {
