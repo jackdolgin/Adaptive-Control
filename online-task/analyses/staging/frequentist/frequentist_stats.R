c6ca12331f42cc75b049332d561022e5a238bcfe
jackdolgin <jdolgin@wustl.edu>
2021-07-28 13:06:49 -0500

    Update frequentist_stats.R

--- online-task/analyses/staging/frequentist/frequentist_stats.R
+++ online-task/analyses/staging/frequentist/frequentist_stats.R
@@ -15,47 +15,76 @@ frequentist_stats <- function(){
     ) %>%
       rep(cautious_regr)
     
+    # if (analysis_type == "difference") {
+    #   options(contrasts = c("contr.treatment","contr.poly"))
+    #   equation <- if_else("Bias" %in% colnames(keys), "~ 1", "~ Bias") %>%
+    #     paste(dv, ., cautious_rand_effects)
+    #   
+    # } else if (analysis_type == "interaction") {
+    #   options(contrasts = c("contr.sum","contr.poly"))                          # apparently interactions do better with contr.sum
+    #   equation <- "~ Congruency * Bias * prev_RT" %>%
+    #     paste(dv, ., "+ (1 | Dominant_Response)", cautious_rand_effects)
+    # }
+    
     if (analysis_type == "difference") {
-      options(contrasts = c("contr.treatment","contr.poly"))
-      equation <- if_else("Bias" %in% colnames(keys), "~ 1", "~ Bias") %>%      # should check this again to make sure (1 | Dominant_Response) shouldn't makes its way in here
-        paste(dv, ., cautious_rand_effects)
+
+      main_effects <- if_else("Bias" %in% colnames(keys), "1", "Bias")
       
     } else if (analysis_type == "interaction") {
-      options(contrasts = c("contr.sum","contr.poly"))                          # apparently interactions do better with contr.sum
-      equation <- "~ Congruency * Bias * prev_RT" %>%
-        paste(dv, ., "+ (1 | Dominant_Response)", cautious_rand_effects)
+
+      main_effects <- "Congruency * Bias * prev_RT"
     }
     
-    if (!(condition == "Locations" & cautious_regr & grepl("RT", dv))){         # so that unique_block and Sub_Code aren't redundant random effects if these three parts of the if statement are all true (would only be redundant in such a case); check if i grepl should or shouldn't positively detect RT_diff
+    equation <- paste(
+      dv, "~", main_effects, cautious_rand_effects, if_else(
+        dv == "RT", "+ (1 | Dominant_Response)", ""
+      )
+    )
+    
+    
+    
+    if (!(condition == "Locations" & cautious_regr)){                           # so that unique_block and Sub_Code aren't redundant random effects if these three parts of the if statement are all true (would only be redundant in such a case)
       equation <- paste(equation, "+ (1 | Sub_Code)")
     }
-
+    
     if (dv == "RT_diff") {
       equation %>%
         lmer(df) %>%
         tidy
     } else if (dv == "RT") {
       
-      fitted <- glmer(
-        equation,
-        df,
-        Gamma(link="identity"),
-        glmerControl(optimizer = "bobyqa")
-      )
       
+      glmer_func <- function(eq){
+        glmer(eq, df, Gamma(link="identity"), glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5)))
+      }
+
+      fitted <- tryCatch(
+        glmer_func(equation),
+        warning = function(w){
+          tryCatch(
+            equation %>%
+              str_remove(" \\* prev_RT") %>%
+              glmer_func,
+            warning = function(w) equation %>%
+              str_remove("\\(1 \\| Dominant_Response\\)") %>%
+              glmer_func
+          )
+        }
+      )
+
       if (analysis_type == "interaction"){
         fitted %>%
           list %>%
-          row_append(paste(condition, analysis_type, sep = "_")) 
-        
+          row_append(paste(condition, analysis_type, sep = "_"))
+
       } else if (analysis_type == "difference"){
         soon_to_be_col_name <- paste(
           condition, "bias", analysis_type, pull(keys, 2), sep = "_")
-        
+
         fitted %>%
           list %>%
           row_append(soon_to_be_col_name)
-        
+
         fitted %>%
           tidy %>%
           filter(str_detect(term, "Bias")) %>%
@@ -67,46 +96,48 @@ frequentist_stats <- function(){
     }
   }
   
-  finished_df %>%
-    mutate(across(Bias, ~ paste("Mostly", Bias))) %>%
-    group_by(Task, Congruency, Bias) %>%
-    group_modify(~ regress_both_tasks(.x, .y, "RT_diff", "difference"))  %>%
-    ungroup() %>%
-    filter(effect == "fixed") %>%
-    mutate(across(Task, ~if_else(. == "Predictive_Blocks", "LWPC", "CSPC")),
-           across(p.value, ~if_else(. < .001, "< .001", paste(round(., 3L)))),
-           across(c(estimate, std.error, statistic, df), ~paste(round(., 3L))),
-           across(c(p.value, estimate, std.error),
-                  ~str_remove(., "0(?=\\.)"))) %>%
-    select(-c(effect, group, term)) %>%
-    rename(
-      Condition = Task,
-      `$\\beta$` = estimate,
-      `\\emph{SE}` = std.error,
-      `\\emph{t}` = statistic,
-      `\\emph{DF}` = df,
-      `\\emph{p}` = p.value) %>%
-    xtable::xtable(
-      align = "llll|rrrrr",
-      caption = stringr::str_wrap(
-        "Linear mixed-effects model estimates of a congruency effect among
+  if (preferred_params){
+    
+    finished_df %>%
+      mutate(across(Bias, ~ paste("Mostly", Bias))) %>%
+      group_by(Task, Congruency, Bias) %>%
+      group_modify(~ regress_both_tasks(.x, .y, "RT_diff", "difference"))  %>%
+      ungroup() %>%
+      filter(effect == "fixed") %>%
+      mutate(across(Task, ~if_else(. == "Predictive_Blocks", "LWPC", "CSPC")),
+             across(p.value, ~if_else(. < .001, "< .001", paste(round(., 3L)))),
+             across(c(estimate, std.error, statistic, df), ~paste(round(., 3L))),
+             across(c(p.value, estimate, std.error),
+                    ~str_remove(., "0(?=\\.)"))) %>%
+      select(-c(effect, group, term)) %>%
+      rename(
+        Condition = Task,
+        `$\\beta$` = estimate,
+        `\\emph{SE}` = std.error,
+        `\\emph{t}` = statistic,
+        `\\emph{DF}` = df,
+        `\\emph{p}` = p.value) %>%
+      xtable::xtable(
+        align = "llll|rrrrr",
+        caption = stringr::str_wrap(
+          "Linear mixed-effects model estimates of a congruency effect among
       each combination of condition, trial congruency, and typical block
       (in LWPC condition) or location (in CSPC condition) congruency,
       using difference-scored RTs"
-      )) %>%
-    list %>%
-    list %>%
-    row_append("table_1")
-  
-  "manuscript" %>%
-    split_violin_builder %>%
-    list %>%
-    row_append("fig_2")
-  
-  # ggsave(split_violin_builder(),
-  #        file = here("online-task", "analyses", "test.svg"),
-  #        width = 10.2, height = 9)
-  
+        )) %>%
+      list %>%
+      list %>%
+      row_append("table_1")
+    
+    "manuscript" %>%
+      split_violin_builder %>%
+      list %>%
+      row_append("fig_2")
+    
+    # ggsave(split_violin_builder(),
+    #        file = here("online-task", "analyses", "test.svg"),
+    #        width = 10.2, height = 9)  
+  }
   
   finished_df %>%
     group_by(Task) %>%
